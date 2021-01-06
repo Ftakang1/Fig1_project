104aff6a06a63fd2a066bda99c83ae4bd21cd237
Frida Takang <63379831+Ftakang1@users.noreply.github.com>
2021-01-06 16:05:10 -0500

    Update terraform-aws-2cs-AZs.tf
    I've been having trouble trying to dynamically assign availability zones to the ec2 instances I create via Terraform.
    
    Context : I've created a shell script to take in user input to specify the number of instances to create. Based on the number of instances, I want to assign each created instance to a specific availability zone using either a for or for_each loop.
    
    shell script
    
    ##create ec2 on AWS
    #!/bin/bash
    
    echo "Starting EC2 Set up..."
    
    echo "Specify number of EC2 instances to create : "
    read number_of_instances
    
    echo "You made a request for $number_of_instances to be created"
    
    terraform validate
    
    terraform apply -var instance_count=${number_of_instances}
    main.tf
    resource "aws_instance" "my-ec2-instance" {
      ami             = var.ami
      count           = var.instance_count
      key_name        = var.key_name
      instance_type   = var.instance_type
      subnet_id       = var.subnet_id
      security_groups = [data.aws_security_group.my-sec-group.id]
    
      availability_zone = ##How do I dynamically assign an availability zone here##
    
      tags = {
        Name    = "my-ec2-instance-tag${count.index + 1}"
        Project = "my terraform project"
      }
    }
    Expected output for my ec2:
    
    If i want 2 instances created
    
    my-ec2-instance1 should be assigned to ap-southeast-1a
    my-ec2-instance2 should be assigned to ap-southeast-1b
    If i want 3 instances created
    
    my-ec2-instance1 should be assigned to ap-southeast-1a
    my-ec2-instance2 should be assigned to ap-southeast-1b
    my-ec2-instance3 should be assigned to ap-southeast-1c
    If i want 5 instances created
    
    my-ec2-instance1 should be assigned to ap-southeast-1a
    my-ec2-instance2 should be assigned to ap-southeast-1b
    my-ec2-instance3 should be assigned to ap-southeast-1c
    my-ec2-instance4 should be assigned to ap-southeast-1a
    my-ec2-instance5 should be assigned to ap-southeast-1b
    What I've tried so far :
    
    variable.tf
    
    variable "availability_zone_map" {
        description = "Availability zone for instance"
        type        = map
        default     = {
            "ap-southeast-1a" = 1 
            "ap-southeast-1b" = 2
            "ap-southeast-1c" = 3
        }
            
    }
    main.tf
    
    resource "aws_instance" "my-ec2-instance" {
      ami             = var.ami
      count           = var.instance_count
      key_name        = var.key_name
      instance_type   = var.instance_type
      subnet_id       = var.subnet_id
      security_groups = [data.aws_security_group.my-sec-group.id]
      for_each        = var.availability_zone_map
      availability_zone = each.key
    
      tags = {
        Name    = "my-ec2-instance-tag${count.index + 1}"
        Project = "my terraform project"
      }
    }
    This is definitely wrong as a resource will not be able to take in count and for_each at the same time.
    
    Would appreciate if anyone could help me get around this? Thank you!

--- terraform-aws-2cs-AZs.tf
+++ terraform-aws-2cs-AZs.tf
@@ -1,1 +1,83 @@
-Using terraform aws to dynamically assign AZs to instances 
+#Using terraform aws to dynamically assign AZs to instances 
+##create ec2 on AWS
+#!/bin/bash
+
+echo "Starting EC2 Set up..."
+
+echo "Specify number of EC2 instances to create : "
+read number_of_instances
+
+echo "You made a request for $number_of_instances to be created"
+
+terraform validate
+
+terraform apply -var instance_count=${number_of_instances}
+
+resource "aws_instance" "my-ec2-instance" {
+  ami             = var.ami
+  count           = var.instance_count
+  key_name        = var.key_name
+  instance_type   = var.instance_type
+  subnet_id       = var.subnet_id
+  security_groups = [data.aws_security_group.my-sec-group.id]
+
+  availability_zone = ##How do I dynamically assign an availability zone here##
+
+  tags = {
+    Name    = "my-ec2-instance-tag${count.index + 1}"
+    Project = "my terraform project"
+  }
+}
+
+variable "availability_zone_map" {
+    description = "Availability zone for instance"
+    type        = map
+    default     = {
+        "ap-southeast-1a" = 1 
+        "ap-southeast-1b" = 2
+        "ap-southeast-1c" = 3
+    }
+        
+}
+
+resource "aws_instance" "my-ec2-instance" {
+  ami             = var.ami
+  count           = var.instance_count
+  key_name        = var.key_name
+  instance_type   = var.instance_type
+  subnet_id       = var.subnet_id
+  security_groups = [data.aws_security_group.my-sec-group.id]
+  for_each        = var.availability_zone_map
+  availability_zone = each.key
+
+  tags = {
+    Name    = "my-ec2-instance-tag${count.index + 1}"
+    Project = "my terraform project"
+  }
+}
+
+variable "availability_zones" {
+    description = "Availability zones for instance"
+    type        = list
+    default     = [
+        "ap-southeast-1a" = 1 
+        "ap-southeast-1b" = 2
+        "ap-southeast-1c" = 3
+    ]
+        
+}
+
+resource "aws_instance" "my-ec2-instance" {
+  ami             = var.ami
+  count           = var.instance_count
+  key_name        = var.key_name
+  instance_type   = var.instance_type
+  subnet_id       = var.subnet_id
+  security_groups = [data.aws_security_group.my-sec-group.id]
+  availability_zone = var.availability_zones[ count %  var.instance_count]
+
+  tags = {
+    Name    = "my-ec2-instance-tag${count.index + 1}"
+    Project = "my terraform project"
+  }
+}
